2fb68c6c8334d9eb3704a23ddafb4433c41eee5a
sjobalia07 <sjobalia07@gmail.com>
2023-02-19 22:47:29 -0800

    Initial Commit

---
+++ MeetUp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MeetUp
+//
+//  Created by Sanjana Jobalia on 19/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MeetUp/MeetUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetUpApp.swift
+//  MeetUp
+//
+//  Created by Sanjana Jobalia on 19/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MeetUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
