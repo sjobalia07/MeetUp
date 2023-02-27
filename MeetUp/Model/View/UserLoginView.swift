3d409eebbdba2cdcb0d6c650f0f0a826556ed867
sjobalia07 <sjobalia07@gmail.com>
2023-02-27 14:37:56 -0800

    Add tabview

---
+++ MeetUp/Model/View/UserLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  UserLoginView.swift
+//  MeetUp
+//
+//  Created by Sanjana Jobalia on 27/02/23.
+//
+
+import SwiftUI
+
+struct UserLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserLoginView()
+    }
+}
